89cad776f28ed674504e660b4a00ba2728becdfe
Yoonha Kim <ivryxx@gmail.com>
2022-04-27 07:53:58 +0900

    add packages/ videoChat complited

--- src/server.js
+++ src/server.js
@@ -1,36 +1,33 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
 import express from 'express';
+
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
 app.get('/*', (_, res) => res.redirect('/'));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-function onSocketClose() {
-  console.log('Disconnected from the Browser ❌');
-}
-const sockets = [];
 
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anon';
-  console.log('Connected to Browser ✅');
-  socket.on('close', onSocketClose);
-  socket.on('message', (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case 'new_message':
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`)
-        );
-      case 'nickname':
-        socket['nickname'] = message.payload;
-    }
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on('connection', (socket) => {
+  socket.on('join_room', (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome');
+  });
+  socket.on('offer', (offer, roomName) => {
+    socket.to(roomName).emit('offer', offer);
+  });
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
+  });
+  socket.on('ice', (ice, roomName) => {
+    socket.to(roomName).emit('ice', ice);
   });
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
